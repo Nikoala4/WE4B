788ff7bbe8c0b9dcd414e6dbbd528fad3d99b016
Simon <simon.menicot@heig-vd.ch>
2025-06-24 16:40:05 +0200

    Add class overview

---
+++ src/app/class-overview/class-overview.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-class-overview',
+  imports: [],
+  templateUrl: './class-overview.component.html',
+  styleUrl: './class-overview.component.scss'
+})
+export class ClassOverviewComponent {
+
+}
